d16613c8029e700f70e758564bb24759ddd97f06
Stealinglight <120747552+Stealinglight@users.noreply.github.com>
2025-12-19 12:52:44 -0800

    Change baseUrl for Docusaurus configuration

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://stealinglight.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/wrist-agent/',
+  baseUrl: '/wrist-agent/intro',
 
   // GitHub pages deployment config.
   organizationName: 'Stealinglight',
