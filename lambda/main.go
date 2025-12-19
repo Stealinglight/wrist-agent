d7a79d4e1dd9b458e712054a0c2fffb7f54526e8
Stealinglight <120747552+Stealinglight@users.noreply.github.com>
2025-12-18 18:58:51 -0800

    code cleanup on lambda

--- lambda/main.go
+++ lambda/main.go
@@ -13,7 +13,6 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime"
 )
 
